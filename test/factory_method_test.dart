96752c380e85a6d9cb39694415da7aefb647cca8
RutananaArnold <arnoldrutanana@gmail.com>
2023-03-24 19:48:00 +0300

    modified the test doc

--- test/factory_method_test.dart
+++ test/factory_method_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 // In this unit test, we use the group and test functions from the test package to group related tests and specify individual tests respectively.
 
-// In each test, we first create an instance of the relevant ProductFactory and use it to create an instance of the corresponding Product. Then, we use the expect function to verify that the created Product is of the correct type and that calling its display method produces the expected output.
+// In each test, we first create an instance of the ProductFactory and use it to create an instance of the corresponding Product. Then, we use the expect function to verify that the created Product is of the correct type and produces the expected output.
 
 // Finally, we can run this unit test using a test runner like flutter test to ensure that the code behaves as expected.
 
